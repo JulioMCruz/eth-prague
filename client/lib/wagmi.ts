ec240ef8358ec531c789f41269ab35e4b9387ba4
Julio M Cruz <julio.cruz@eb-ms.net>
2025-05-31 02:52:00 +0200

    adding explore page

--- client/lib/wagmi.ts
+++ client/lib/wagmi.ts
@@ -2,7 +2,6 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
   flare,
-  flareTestnet,
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
@@ -10,8 +9,6 @@ export const config = getDefaultConfig({
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '',
   chains: [
     flare,
-    flareTestnet,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [flareTestnet] : []),
   ],
   ssr: true,
 });
